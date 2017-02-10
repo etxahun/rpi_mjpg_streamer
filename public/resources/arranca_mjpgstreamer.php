ff86af6a6f696e21b3c25574d21dd89c2efcd315
Etxahun Sanchez Bazterretxea <etxahun.sanchez@gmail.com>
2017-02-10 10:16:25 +0100

    Update arranca_mjpgstreamer.php

--- public/resources/arranca_mjpgstreamer.php
+++ public/resources/arranca_mjpgstreamer.php
@@ -1,6 +1,5 @@
 <?php
-//shell_exec("sudo sh /home/pi/01-arrancar_raspistill_y_mjpgstreamer.sh");
-$file = fopen("/root/odamae/odamaeweb_mjpg-streamer/public/resources/estado_mjpgstreamer.txt", "w") or die("Unable to open file!");
+$file = fopen("/home/pi/rpi_mjpg_streamer/public/resources/estado_mjpgstreamer.txt", "w") or die("Unable to open file!");
 fwrite($file, "1");
 fclose($file);
 ?>
