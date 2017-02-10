98d4610f9fd1e0181e28416d6e7b58f2b0305421
Etxahun Sanchez Bazterretxea <etxahun.sanchez@gmail.com>
2017-02-10 10:16:59 +0100

    Update lee_estado_mjpgstreamer.php

--- public/resources/lee_estado_mjpgstreamer.php
+++ public/resources/lee_estado_mjpgstreamer.php
@@ -1,5 +1,5 @@
 <?php
-$file = fopen("/root/odamae/odamaeweb_mjpg-streamer/public/resources/estado_mjpgstreamer.txt", "r") or die("Unable to open estado_mjpgstreamer.txt file!");
+$file = fopen("/home/pi/rpi_mjpg_streamer/public/resources/estado_mjpgstreamer.txt", "r") or die("Unable to open estado_mjpgstreamer.txt file!");
 echo fgetc($file);
 fclose($file);
 ?>
