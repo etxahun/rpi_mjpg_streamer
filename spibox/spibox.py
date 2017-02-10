f8bbbac09e3b8970db478365f87247c38440075a
Etxahun Sanchez Bazterretxea <etxahun.sanchez@gmail.com>
2017-02-10 10:07:38 +0100

    Update spibox.py

--- spibox/spibox.py
+++ spibox/spibox.py
@@ -12,7 +12,7 @@ def photo():
     for i in range(1,2):
         capturename = get_file_name()
         print('Motion detected! Taking snapshot')
-        cmd="raspistill -w 640 -h 480 -rot 270 -n -t 10 -q 10 -e jpg -th none -o /home/odamae/odamaeweb_mjpg-streamer/resources/photos/" + capturename+"_%d.jpg" % (i)
+        cmd="raspistill -w 640 -h 480 -rot 270 -n -t 10 -q 10 -e jpg -th none -o /home/pi/rpi_mjpg_streamer/resources/photos/" + capturename+"_%d.jpg" % (i)
         #cmd="raspistill -rot 270 -o /home/odamae/spibox/capture/" + capturename+"_%d.jpg" % (i)
         camerapid = subprocess.call(cmd,shell=True)
 
