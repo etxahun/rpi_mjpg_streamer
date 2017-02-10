8af6c8991ad57f893fb1dcea023660139cb4cb27
Etxahun Sanchez Bazterretxea <etxahun.sanchez@gmail.com>
2017-02-10 10:31:19 +0100

    Update camera_start.sh

--- mjpg-streamer/camera_start.sh
+++ mjpg-streamer/camera_start.sh
@@ -11,11 +11,11 @@ echo
 
 # Guardar el estado:
 
-echo -n 1 > "/root/odamae/odamaeweb_mjpg-streamer/public/resources/estado_mjpgstreamer.txt"
+echo -n 1 > "/home/pi/rpi_mjpg_streamer/public/resources/estado_mjpgstreamer.txt"
  
 # Start Motion JPEG
  
 cd /usr/src/mjpg-streamer/mjpg-streamer-experimental/
 export LD_LIBRARY_PATH=.
 # ./mjpg_streamer -o "output_http.so -w /home/odamae/prueba/camera/www -p 8081" -i "input_raspicam.so -rot 270 -fps 15 -q 50 -x 640 -y 480 ex night"
-./mjpg_streamer -o "output_http.so -w /home/odamae/odamaeweb_mjpg-streamer/mjpg-streamer/www -p 8081 -c kakafu:kakafu" -i "input_raspicam.so -rot 270 -fps 25 -q 50 -x 320 -y 240 ex night"
+./mjpg_streamer -o "output_http.so -w /home/pi/rpi_mjpg_streamer/mjpg-streamer/www -p 8081 -c kakafu:kakafu" -i "input_raspicam.so -rot 270 -fps 25 -q 50 -x 320 -y 240 ex night"
