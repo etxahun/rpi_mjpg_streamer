fa390cbb1f1a2e86816f912f176808222e0fbfc7
Etxahun Sanchez Bazterretxea <etxahun.sanchez@gmail.com>
2017-02-10 10:23:41 +0100

    Update lee_estado_spibox.php

--- public/resources/lee_estado_spibox.php
+++ public/resources/lee_estado_spibox.php
@@ -1,5 +1,5 @@
 <?php
-$file = fopen("/root/odamae/odamaeweb_mjpg-streamer/public/resources/estado_spibox.txt", "r") or die("Unable to open estado_spibox.txt file!");
+$file = fopen("/home/pi/rpi_mjpg_streamer/public/resources/estado_spibox.txt", "r") or die("Unable to open estado_spibox.txt file!");
 echo fgetc($file);
 fclose($file);
 ?>
